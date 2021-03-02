3974c08d9088eb374724cc8b17c343018131fc24
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-03-02 13:13:45 -0500

    added validations

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,4 +1,12 @@
 class Festival < ApplicationRecord
   belongs_to :user
   has_many :festival_policies
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :festival_type, presence: true
+  validates :name, presence: true
+  validates :venue, presence: true
+
+  # ? Validate longitude/latitude?
 end
