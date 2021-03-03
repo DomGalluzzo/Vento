96a29f94b2808034ff7f11e9ed150b95538a8633
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-03-03 13:29:19 -0500

    added columns to festivals

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,3 +1,5 @@
 class Festival < ApplicationRecord
   belongs_to :user
+
+  validates :start_date, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_011658) do
+ActiveRecord::Schema.define(version: 2021_03_03_182532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,9 @@ ActiveRecord::Schema.define(version: 2021_03_03_011658) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "genre"
+    t.string "name"
+    t.string "venue"
     t.index ["user_id"], name: "index_festivals_on_user_id"
   end
 
