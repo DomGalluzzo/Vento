7085787fef153b39c8c894c1b8df3385d2bab9d4
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-03-03 13:42:20 -0500

    added validations to models

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -2,4 +2,8 @@ class Festival < ApplicationRecord
   belongs_to :user
 
   validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :genre, presence: true
+  validates :name, presence: true
+  validates :venue, presence: true
 end
