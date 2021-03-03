44ece4bd194fee313d1c45fbd9198324e56fff55
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-03-03 15:15:19 -0500

    controller actions for contract plans, contracts, and festivals

--- app/controllers/contract_plans_controller.rb
+++ app/controllers/contract_plans_controller.rb
@@ -1,10 +1,15 @@
 class ContractPlansController < ApplicationController
   def new
+    @contract = Contract.find(params[:contract_id])
     @contract_plan = ContractPlan.new
   end
 
   def create
-    @contract_plan = ContractPlan.new(params[:contract_plan])
+    @contract_plan = ContractPlan.new(contract_plan_params)
+    @contract = Contract.find(params[:contract_id])
+
+    # ? Add plannable_id ?
+
     if @contract_plan.save
       flash[:success] = "ContractPlan successfully created"
       redirect_to @contract_plan
@@ -12,5 +17,11 @@ class ContractPlansController < ApplicationController
       flash[:error] = "Something went wrong"
       render 'new'
     end
+  end
+
+  private
+
+  def contract_plan_params
+    params.require(:contract_plan).permit(:contract_id, :plannable_id, :plannable_type)
   end
 end
--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,7 +1,23 @@
 class FestivalsController < ApplicationController
   def new
+    @festival = Festival.new
   end
 
   def create
+    @festival = Festival.new(festival_params)
+    @festival.user = current_user
+    if @festival.save
+      flash[:success] = "Festival successfully created"
+      redirect_to @festival
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private
+
+  def festival_params
+    params.require(:fetival).permit(:user_id, :start_date, :end_date, :genre, :name, :venue)
   end
 end
