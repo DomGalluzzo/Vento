aaa7b82d6b547cb8bba5f63ff89ed41e384d2cf5
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-03-04 16:16:10 -0500

    added helper to application_helper. Dashboard now shows all relevant attributes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # On dashboard, changes user's answers from forms to display "yes" or "no" depending on boolean
+  def check_bool(answer)
+    if answer.eql?(true)
+      "Yes"
+    else
+      "No"
+    end
+  end
 end
