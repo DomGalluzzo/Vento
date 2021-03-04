ed0299d793e32683b3582fe840dd59d0c85276e7
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-03-04 16:45:39 -0500

    updated seed file with 2 different insurance contracts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,22 @@ Contract.destroy_all
 Festival.destroy_all
 User.destroy_all
 
-dom = User.create!(email: "dom123.galluzzo@gmail.com", password: "111111")
-fest = Festival.create!(start_date: DateTime.new, end_date: DateTime.new, genre: "Music", name: "Cool Festival", venue: "Cool Venue", user: dom)
+dom1 = User.create!(email: "dom2.galluzzo@gmail.com", password: "111111")
+dom2 = User.create!(email: "dom1.galluzzo@gmail.com", password: "111111")
 
-contract1 = Contract.create!(total_cost: 10000, festival: fest)
+fest1 = Festival.create!(start_date: DateTime.new(2021,1,1), end_date: DateTime.new(2021,1,2), genre: "Music", name: "Cool Festival", venue: "Cool Venue", user: dom1)
+fest2 = Festival.create!(start_date: DateTime.new(2021,2,2), end_date: DateTime.new(2021,2,3), genre: "Race", name: "Cool Race", venue: "Cool Race Place", user: dom2)
+
+contract1 = Contract.create!(total_cost: 10000, festival: fest1)
+contract2 = Contract.create!(total_cost: 5000, festival: fest2)
 
 injury1 = PublicInjury.create!(liability: 100, daily: 10, total: 15, usa: true, dangerous: true, sub: true)
 injury2 = PublicInjury.create!(liability: 200, daily: 20, total: 25, usa: true, dangerous: true, sub: true)
-cancel = Cancellation.create!(budget: 100, terrorism: false, location: "London", rain: true, flood: false, loss: true, weather: true)
+
+cancel1 = Cancellation.create!(budget: 100, terrorism: false, location: "Outdoors", rain: true, flood: false, loss: true, weather: true)
+cancel2 = Cancellation.create!(budget: 100, terrorism: false, location: "Outdoors", rain: true, flood: false, loss: true, weather: true)
 
 contract_plan1 = ContractPlan.create!(contract: contract1, plannable: injury1)
-contract1.contract_plans.create!(plannable: cancel)
+contract1.contract_plans.create!(plannable: cancel1)
+
+contract_plan2 = ContractPlan.create!(contract: contract2, plannable: cancel2)
