5a6618c58aaf8157ddc135d9f536427ed5ba9022
Freddie Sparrow <freddie.sparrow@gmail.com>
2021-03-09 19:18:02 -0600

    started stripe

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,4 +3,13 @@ class Contract < ApplicationRecord
   has_many :contract_plans
   has_many :cancellations, through: :contract_plans, source: :plannable, source_type: 'Cancellation'
   has_many :public_injuries, through: :contract_plans, source: :plannable, source_type: 'PublicInjury'
+
+  def price_sum
+    total = 0
+    self.contract_plans.each do |p|
+      total += p.plannable.price
+    end
+    self.price = total
+    self.save
+  end
 end
