c70431d789fb071129871c477fda4b1430fca30e
Eduardo Acosta <eduardo.am7049@gmail.com>
2021-03-12 12:36:07 -0600

    List Animation Done

--- app/javascript/packs/list_animation.js
+++ app/javascript/packs/list_animation.js
@@ -2,7 +2,7 @@ const animation = () => {
     const items = document.querySelectorAll(".group-item")
     document.addEventListener("readystatechange", () => {
         items.forEach(item => {
-            item.style.marginLeft = "0px";
+            item.style.marginLeft = "50px";
         })
     })
 }
